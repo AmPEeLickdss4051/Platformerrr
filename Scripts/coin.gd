d27c37ab6f29354de16b01790e2d6d746a372bf8
AmPEeLickdss4051 <tef405@mail.ru>
2025-10-05 23:38:50 +1000

    khe

---
+++ Scripts/coin.gd
@@ -0,0 +1,10 @@
+extends Area3D
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position + Vector3(0, 0.5, 0), 0.3)
+		tween.tween_callback(queue_free)
+		body.coin += 1
+		print(body.coin)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,10 @@ extends CharacterBody3D
 @export var SPEED = 5.0
 @export var JUMP_VELOCITY = 4.5
 
+@onready var skin: Node3D = $Skin
 @onready var camera: Camera3D = $SpringArm3D/Camera3D
+
+var coin: float
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,11 +20,20 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	direction = direction.rotated(Vector3.UP, camera.global_rotation.y)
-	if direction:
+	if direction != Vector3.ZERO:
+		#move
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		#rotate
+		var tar_position = global_position + direction
+		var cur_rotation = skin.global_rotation
+		skin.look_at(tar_position, Vector3.UP)
+		skin.global_rotation.y = lerp_angle(cur_rotation.y, skin.global_rotation.y, 0.2)
+		
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
 		
 	move_and_slide()
