1751fef45cfd75aa962fd3b1976c48d81407ac8d
AmPEeLickdss4051 <tef405@mail.ru>
2025-10-18 10:31:48 +1000

    87658765

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,10 +1,12 @@
 extends Area3D
 
 @onready var particl: GPUParticles3D = $GPUParticles3D
+@onready var particl2: GPUParticles3D = $GPUParticles3D2
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
 		particl.emitting = true
+		particl2.emitting = false
 		var tween = get_tree().create_tween()
 		tween.tween_property(self, "position", position + Vector3(0, 0.3, 0), 0.5)
 		tween.tween_callback(queue_free)
