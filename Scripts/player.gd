a3bbb12cfabfb01ca61291b9ba8c27b948eaafc2
AmPEeLickdss4051 <tef405@mail.ru>
2025-10-20 22:45:00 +1000

    zxc

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,16 +5,30 @@ extends CharacterBody3D
 @export var JUMP_VELOCITY = 3.5
 @export var JUMP_ACC = 3.8
 @export var ACCELERATION = 4.0
+@export var BLEND_SPEED = 15.0
 
-@onready var skin: Node3D = $Skin
+@onready var character: Node3D = $character
 @onready var camera: Camera3D = $CameraPivot/SpringArm3D/Camera3D
 @onready var health_bar = $UI_Player/TextureProgressBar
-
+@onready var animation_tree: AnimationTree = $character/AnimationTree
 
 var direction := Vector3.ZERO
 var coin: int
 var doubleJumpActivated = true
 var can_Action = false
+
+
+enum {IDLE, RUN, ACC}
+var currentAnim = IDLE
+
+func handle_anim(delta):
+	match  currentAnim:
+		IDLE:
+			animation_tree.set("parameters/Movement/transition_request", "Idle")
+		RUN:
+			animation_tree.set("parameters/Movement/transition_request", "SlowRun")
+		ACC:
+			animation_tree.set("parameters/Movement/transition_request", "FastRun")
 
 func _ready():
 	Signals.Damage.connect(death)
@@ -26,6 +40,7 @@ func death(hit):
 		get_tree().quit()
 		
 func _physics_process(delta: float) -> void:
+	handle_anim(delta)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -34,8 +49,8 @@ func _physics_process(delta: float) -> void:
 		doubleJumpActivated = true
 	
 	var input_dir := Input.get_vector("LEFT", "RIGHT", "UP", "DOWN")
-	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	direction = direction.rotated(Vector3.UP, camera.global_rotation.y)
+	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y))
+	direction = direction.rotated(Vector3.UP, camera.global_rotation.y).normalized()
 	
 	if Input.is_action_just_pressed("Pause") or coin == 5:
 		Input.mouse_mode = Input.MOUSE_MODE_CONFINED
@@ -46,44 +61,44 @@ func _physics_process(delta: float) -> void:
 		doubleJumpActivated = true
 
 func idle():
-	pass
+	velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.z = move_toward(velocity.z, 0, SPEED)
+	currentAnim = IDLE
 
 func move():
 	if direction != Vector3.ZERO:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		var tar_position = global_position + direction
-		var cur_rotation = skin.global_rotation
-		skin.look_at(tar_position, Vector3.UP)
-		skin.global_rotation.y = lerp_angle(cur_rotation.y, skin.global_rotation.y, 0.2)
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		currentAnim = RUN
+		
+		var target_angel = atan2(direction.x, direction.z)
+		var current_angel = character.global_rotation.y
+		character.global_rotation.y = lerp_angle(current_angel, target_angel, 0.3)
 	
 func acceleration():
 	if direction != Vector3.ZERO:
 		velocity.x = direction.x * ACCELERATION
 		velocity.z = direction.z * ACCELERATION
-		var tar_position = global_position + direction
-		var cur_rotation = skin.global_rotation
-		skin.look_at(tar_position, Vector3.UP)
-		skin.global_rotation.y = lerp_angle(cur_rotation.y, skin.global_rotation.y, 0.2)
-	else:
-		velocity.x = move_toward(velocity.x, 0, ACCELERATION)
-		velocity.z = move_toward(velocity.z, 0, ACCELERATION)
+		currentAnim = ACC
+		
+		var target_angel = atan2(direction.x, direction.z)
+		var current_angel = character.global_rotation.y
+		character.global_rotation.y = lerp_angle(current_angel, target_angel, 0.2)
 	
 func jump():
 	velocity.y = JUMP_VELOCITY
+	animation_tree.set("parameters/JUMP/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
 func jump_acc():
 	velocity.y = JUMP_ACC
 	velocity.x = direction.x * ACCELERATION
 	velocity.z = direction.z * ACCELERATION
+	animation_tree.set("parameters/JUMP/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 	
 func double_jump():
 	velocity.y = JUMP_VELOCITY
 	doubleJumpActivated = false
-	
+	animation_tree.set("parameters/JUMP/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
